665cc452e3d8a382d22177d83d61ed775a2219ee
0xdya <0xdya@proton.me>
2026-04-16 14:07:04 +0100

    اضافة التعليقات

--- docs/js/script.js
+++ docs/js/script.js
@@ -15,3 +15,33 @@ themeToggle.addEventListener('click', () => {
         localStorage.setItem('theme', 'black');
     }
 });
+
+
+function loadGiscus() {
+    const giscusContainer = document.querySelector('.giscus');
+    if (!giscusContainer) return;
+
+    const themeUrl = window.location.origin + '/css/giscus-theme.css';
+
+    const script = document.createElement('script');
+    script.src = "https://giscus.app/client.js";
+    script.setAttribute('data-repo', "0xdya/d1wiki");
+    script.setAttribute('data-repo-id', "R_kgDOPqo2eQ");
+    script.setAttribute('data-category', "General");
+    script.setAttribute('data-category-id', "DIC_kwDOPqo2ec4CieW5");p
+    script.setAttribute('data-mapping', "pathname");
+    script.setAttribute('data-strict', "1");
+    script.setAttribute('data-reactions-enabled', "0");
+    script.setAttribute('data-emit-metadata', "0");
+    script.setAttribute('data-input-position', "bottom");
+    
+    script.setAttribute('data-theme', themeUrl);
+    
+    script.setAttribute('data-lang', "ar");
+    script.setAttribute('crossorigin', "anonymous");
+    script.async = true;
+
+    document.body.appendChild(script);
+}
+
+document.addEventListener('DOMContentLoaded', loadGiscus);
